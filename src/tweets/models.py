426d42deb2f858d793b24e48d32b90af5b634f9d
Fan <aprilzhang1994@gmail.com>
2017-10-16 11:55:05 -0400

    first commit

---
+++ src/tweets/models.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.conf import settings
+from django.db import models
+
+# Create your models here.
+
+class Tweet(models.Model):
+	user 		= models.ForeignKey(settings.AUTH_USER_MODEL, default=1)
+	content 	= models.CharField(max_length=140)
+	updated 	= models.DateTimeField(auto_now=True)
+	timestamp 	= models.DateTimeField(auto_now_add=True)
+
+	def __str__(self):
+		return str(self.content)
